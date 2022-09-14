37359cc2e9bf5f508f7c2264e92dc47e46356e10
Arpita Jaiswal <ayushipujaa@gmail.com>
2022-09-14 20:25:22 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 /bin/sh fpm-install.sh
 
-./fpm serve $PORT --bind 0.0.0.0 --download-base-url https://raw.githubusercontent.com/<repo-owner>/<repo-name>/<branch-name>/
+./fpm serve $PORT --bind 0.0.0.0 --download-base-url $DOWNLOAD_BASE_URL
